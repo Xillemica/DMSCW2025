bbaca69503ef5a956e58e10009ae6bd432c21f52
Rebecca <rebeccapereira06@gmail.com>
2025-11-30 22:05:57 +0800

    refactored Main.java: - extracted the startup logic into a separate method for cleaner structure. - removed unused ResourceBundle and import. - replaced magic numbers with named constants - renamed variable ‘c’ to ‘guiController’ for better clarity.

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -1,30 +1,35 @@
 package com.comp2042;
 
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class Main extends Application {
+
+    private static final int SCENE_WIDTH = 300;
+    private static final int SCENE_HEIGHT = 510;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setupStage(primaryStage);
+    }
 
+    private void setupStage(Stage primaryStage) throws Exception {
         URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
-        ResourceBundle resources = null;
-        FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
         Parent root = fxmlLoader.load();
-        GuiController c = fxmlLoader.getController();
+        GuiController guiController = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
         primaryStage.setScene(scene);
         primaryStage.show();
-        new GameController(c);
+
+        new GameController(guiController);
     }
 
 
