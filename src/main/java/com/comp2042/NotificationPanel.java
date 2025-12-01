da110e0b83c387ced7b3c440f304fdcbe94e5c53
Rebecca <rebeccapereira06@gmail.com>
2025-12-02 02:57:38 +0800

    refactor NotificationPanel.java: -improved variable names and removed unused imports - splitted animation stuff into helper methods -replaced magic numbers with constants - replaced anonymous class for onFinished  with lambda

--- src/main/java/com/comp2042/NotificationPanel.java
+++ src/main/java/com/comp2042/NotificationPanel.java
@@ -4,43 +4,47 @@ import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
 import javafx.animation.TranslateTransition;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class NotificationPanel extends BorderPane {
 
+    private static final int FADE_DURATION_MS = 2000;
+    private static final int TRANSLATE_DURATION_MS = 2500;
+    private static final int MOVE_OFFSET_Y = -40;
+
     public NotificationPanel(String text) {
         setMinHeight(200);
         setMinWidth(220);
-        final Label score = new Label(text);
-        score.getStyleClass().add("bonusStyle");
-        final Effect glow = new Glow(0.6);
-        score.setEffect(glow);
-        score.setTextFill(Color.WHITE);
-        setCenter(score);
 
+        Label label = new Label(text);
+        label.getStyleClass().add("bonusStyle");
+        label.setEffect(new Glow(0.6));
+        setCenter(label);
     }
 
-    public void showScore(ObservableList<Node> list) {
-        FadeTransition ft = new FadeTransition(Duration.millis(2000), this);
-        TranslateTransition tt = new TranslateTransition(Duration.millis(2500), this);
-        tt.setToY(this.getLayoutY() - 40);
-        ft.setFromValue(1);
-        ft.setToValue(0);
-        ParallelTransition transition = new ParallelTransition(tt, ft);
-        transition.setOnFinished(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                list.remove(NotificationPanel.this);
-            }
-        });
-        transition.play();
+    public void showScore(ObservableList<Node> parentList) {
+        FadeTransition fade = createFadeTransition();
+        TranslateTransition translate = createTranslateTransition();
+
+        ParallelTransition animation = new ParallelTransition(translate, fade);
+        animation.setOnFinished(e -> parentList.remove(NotificationPanel.this));
+        animation.play();
+    }
+
+    private FadeTransition createFadeTransition() {
+        FadeTransition fade = new FadeTransition(Duration.millis(FADE_DURATION_MS), this);
+        fade.setFromValue(1);
+        fade.setToValue(0);
+        return fade;
+    }
+
+    private TranslateTransition createTranslateTransition() {
+        TranslateTransition translate = new TranslateTransition(Duration.millis(TRANSLATE_DURATION_MS), this);
+        translate.setToY(getLayoutY() + MOVE_OFFSET_Y);
+        return translate;
     }
 }
